06a87d42ea3b583b4a7ab1b6c003616dcc4d2a19
digoro <lsh00124@naver.com>
2019-01-25 23:58:48 +0900

    Remove authentication message

--- src/app/service/guard/auth-guard.ts
+++ src/app/service/guard/auth-guard.ts
@@ -34,8 +34,7 @@ export class AuthGuard implements CanActivateChild {
       header: '로그인 해주세요',
       message: '알라머 서비스를 이용하려면 로그인이 필요합니다.',
       buttons: [
-        { text: '확인', handler: () => this.router.navigate(['tabs/main']) },
-        { text: '취소' }
+        { text: '확인', handler: () => this.router.navigate(['tabs/main']) }
       ]
     });
     return await alert.present();
